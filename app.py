41128f5a3526915fb9102c4c5eab49eb4b566918
xchbx <ceina.ellison@gmail.com>
2025-03-30 09:12:16 -0400

    added quicksort quix and created json file

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
+
+def load_quiz_answers():
+  try:
+    with open('quiz_data.json', 'r') as file:
+      return json.load(file)
+  except FileNotFoundError:
+    return []
+  
+quiz_data = load_quiz_answers()
 
 @app.route('/')
 def home():
@@ -23,6 +33,16 @@ def quicksort():
 @app.route('/quicksortQuiz', methods=['GET'])
 def quicksortQuiz():
   return render_template('/quicksortQuiz.html')
+
+@app.route('/submit_quicksort_quiz', methods=['POST'])
+def quicksortAnswers():
+  user_answers = {key: request.form.get(key, "") for key in ["question1", "question2", "question3", "question4", "question5"]}
+  quicksort_correct_answers = next((item for item in quiz_data if item["id"] == "Quicksort Answer"), None)
+  if quicksort_correct_answers:
+    return render_template('/quicksortAnswer.html', user_answers=user_answers, quicksort_answers=quicksort_correct_answers)
+  else:
+    return "Quicksort answers not found.", 404
+
 
 if __name__ == '__main__':
   app.run(debug=True)
